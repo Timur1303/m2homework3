409b2e27214889ae30c4d688197d554f712d7cfa
Timur1303 <tima130394@gmail.com>
2020-05-09 01:46:23 +0600

    v

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ public class Main {
         for (int i = 0;  ; i++) {
             try {
                 bank.withDraw(6000.00);
-                System.out.println("Снять 6000");
+                System.out.println("Снят 6000");
             } catch (LimitException b) {
                 System.out.println("Снять оставшую сумму " + b.getRemainingAmount());
                 break;
